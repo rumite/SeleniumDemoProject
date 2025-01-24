646b21a43c7e9456ca8613877e5eb44196a1c672
rumite <bakhtiyarowtn@gmail.com>
2025-01-24 09:24:01 +0400

    Browser options pt 1

---
+++ src/test/java/browsers/BrowserOptions.java
@@ -0,0 +1,15 @@
+package browsers;
+
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class BrowserOptions {
+    public void chromeBrowserProfiling() {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setBinary("/Users/timur/Downloads/chrome-mac-arm64/Google Chrome for Testing.app");
+
+    }
+
+    public void firefoxBrowserProfiling() {
+
+    }
+}
